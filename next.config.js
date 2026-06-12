59a4f286cd27d940c50c610b9a1dce2fa8129d7f
Srivatsa Marichi <srivatsams@yahoo.com>
2026-06-12 15:38:58 +0530

    Perf: Set images.unoptimized to true to fix hostname errors

--- next.config.js
+++ next.config.js
@@ -1,44 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'anandsp1.wordpress.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i1.sndcdn.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'blogger.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.umath.in',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.sripadarajamutt.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'd18x2uyjeekruj.cloudfront.net',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
