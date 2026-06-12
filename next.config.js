f03003c6566cd84af9c4171dca87f2669c04617c
Srivatsa Marichi <srivatsams@yahoo.com>
2026-06-12 15:24:37 +0530

    Fix: Add 'i.pinimg.com' to next/image remote patterns

--- next.config.js
+++ next.config.js
@@ -22,6 +22,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'blogger.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
     ],
   },
 }
