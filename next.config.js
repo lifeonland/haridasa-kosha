336d4a6749b12ba2acc624809f83a21ee699ba59
Srivatsa Marichi <srivatsams@yahoo.com>
2026-06-17 15:06:27 +0530

    fix: add all external image domains to next/image remotePatterns

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
-      },
+      { protocol: 'https', hostname: 'i.pinimg.com' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org' },
+      { protocol: 'https', hostname: 'blogger.googleusercontent.com' },
+      { protocol: 'https', hostname: 'anandsp1.wordpress.com' },
+      { protocol: 'https', hostname: 'i1.sndcdn.com' },
+      { protocol: 'https', hostname: 'cdn.umath.in' },
+      { protocol: 'https', hostname: 'd18x2uyjeekruj.cloudfront.net' },
+      { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com' },
+      { protocol: 'https', hostname: 'www.sripadarajamutt.org' },
     ],
   },
 }
